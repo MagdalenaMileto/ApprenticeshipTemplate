304325e01e27b2c5391c547978c2b69b8328ec76
MagdalenaMileto <maggiemileto@gmail.com>
2017-04-09 21:26:30 -0300

    test3: given a random position, returns the correct number

--- lib/fibonacci_sequence.rb
+++ lib/fibonacci_sequence.rb
@@ -1,6 +1,10 @@
 class FibonacciSequence
 
   def number_for(position)
+    if position > 1
+      number_for(position - 2) + number_for(position - 1)
+    else
     position
+    end
   end
 end
--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe FibonacciSequence do
-  let(:fibonacci) {FibonacciSequence.new}
+  let(:fibonacci) { FibonacciSequence.new }
   context 'when the position is 0' do
     it 'should return 0' do
       expect(fibonacci.number_for(0)).to eq(0)
@@ -13,4 +13,11 @@ describe FibonacciSequence do
       expect(fibonacci.number_for(1)).to eq(1)
     end
   end
+
+  context 'given a random position' do
+    it 'should return the number for that position' do
+      expect(fibonacci.number_for(12)).to eq(144)
+    end
+  end
 end
+

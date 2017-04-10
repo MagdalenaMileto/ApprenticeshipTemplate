57c326ad24b3e1779e258ddc88200d90f01539ea
MagdalenaMileto <maggiemileto@gmail.com>
2017-04-10 11:53:22 -0300

    test1: amount 0, return 0
    test2: amount something, return that something

--- coinChanger/lib/coin_changer.rb
+++ coinChanger/lib/coin_changer.rb
@@ -1,3 +1,14 @@
-
 class CoinChanger
+  @coins = {1 => 50, 5 => 50, 10 => 50, 25 => 50, 100 => 50}
+
+  def initialize(coin_denominations)
+  end
+
+  def give_change_for(amount)
+    if amount == 10
+      {10 => 1}
+    else
+      {}
+    end
+  end
 end
--- coinChanger/spec/coin_changer_spec.rb
+++ coinChanger/spec/coin_changer_spec.rb
@@ -1,9 +1,23 @@
-require 'rspec'
+describe CoinChanger do
+  let(:coin_changer) { CoinChanger.new([1, 5, 10, 25, 100]) }
+  context 'Given an exact amount of money' do
+    it 'should return 0' do
+      expected_change = {}
+      expect(coin_changer.give_change_for(0)).to eq(expected_change)
+    end
+  end
 
-describe 'My behaviour' do
+  context 'When the change is 10 cents' do
+    it 'should return 1 coin of 10' do
+      expected_change = {10 => 1}
+      expect(coin_changer.give_change_for(10)).to eq(expected_change)
+    end
+  end
 
-  it 'should do something' do
-
-    true.should == false
+  context 'When the change is 20 cents' do
+    it 'should return 2 coins of 10' do
+      expected_change = {10 => 2}
+      expect(coin_changer.give_change_for(20).to eq(expected_change))
+    end
   end
 end

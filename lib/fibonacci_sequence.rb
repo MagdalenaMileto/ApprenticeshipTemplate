8e8121b4c1bfec2ef05f15752611767839a47728
MagdalenaMileto <maggiemileto@gmail.com>
2017-04-09 23:14:00 -0300

    test4: given a negative number, raise exception

--- lib/fibonacci_sequence.rb
+++ lib/fibonacci_sequence.rb
@@ -1,6 +1,7 @@
 class FibonacciSequence
 
   def number_for(position)
+    raise 'the position must be positive' unless position >= 0
     if position > 1
       number_for(position - 2) + number_for(position - 1)
     else
--- spec/sequence_spec.rb
+++ spec/sequence_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe FibonacciSequence do
+
   let(:fibonacci) { FibonacciSequence.new }
   context 'when the position is 0' do
     it 'should return 0' do
@@ -17,6 +18,12 @@ describe FibonacciSequence do
   context 'given a random position' do
     it 'should return the number for that position' do
       expect(fibonacci.number_for(12)).to eq(144)
+    end
+  end
+
+  context 'given a negative position' do
+    it 'should raise and exception' do
+      expect{fibonacci.number_for(-3)}.to raise_error('the position must be positive')
     end
   end
 end

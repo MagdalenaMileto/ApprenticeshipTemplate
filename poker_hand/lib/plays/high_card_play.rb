1f4ae3e12cae0d9b1e948cb838109a39b69c61ca
Maggie <maggiemileto@gmail.com>
2017-04-21 17:22:48 -0300

    Test006 - High value play

---
+++ poker_hand/lib/plays/high_card_play.rb
@@ -0,0 +1,7 @@
+class HighCardPlay
+
+  def who_wins(hand1, _hand2)
+    hand1
+  end
+
+end
--- poker_hand/spec/hand_of_cards_spec.rb
+++ poker_hand/spec/hand_of_cards_spec.rb
@@ -6,7 +6,7 @@ describe 'Hands' do
   let(:card4) { PokerCard.new(PokerCard.values('4'), :C) }
   let(:card5) { PokerCard.new(PokerCard.values('As'), :C) }
   let(:hand1) { Hand.new([card1, card2, card3, card4, card5]) }
-  let(:hand2) { Hand.new([card1, card2, card3, card4]) }
+  let(:hand2) { Hand.new([card1, card2, card3, card4, card4]) }
 
   describe '#valid?' do
     context 'given five cards to a hand' do
@@ -17,9 +17,19 @@ describe 'Hands' do
   end
 
   describe '#valid?' do
+    let(:invalid_hand) { Hand.new([card1, card2, card3, card4]) }
     context 'given five cards to a hand' do
       it 'return true' do
-        expect { (hand2.valid?) }.to raise_error('Invalid number of cards, they must be 5')
+        expect { (invalid_hand.valid?) }.to raise_error('Invalid number of cards, they must be 5')
+      end
+    end
+  end
+
+  describe '#who_wins' do
+    let(:high_card_play) { HighCardPlay.new }
+    context 'given two hands to make high value play' do
+      it 'return the highest' do
+        expect(high_card_play.who_wins(hand1, hand2)).to be(hand1)
       end
     end
   end

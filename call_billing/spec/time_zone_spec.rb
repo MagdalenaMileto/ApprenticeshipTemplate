2419a6a4454fd34bcc8270bebda0c97698bf37b0
Maggie <maggiemileto@gmail.com>
2017-05-02 14:40:50 -0300

    test004: price_per_minute returns price while 0.10

--- call_billing/spec/time_zone_spec.rb
+++ call_billing/spec/time_zone_spec.rb
@@ -3,8 +3,15 @@ require 'rspec'
 describe TimeZone do
   let(:time_zone_rush_hour) { TimeZone.new(0.20) }
   describe '#price_per_minute' do
-    it 'returns the price' do
+    it 'returns 0.20' do
       expect(time_zone_rush_hour.price_per_minute).to eq(0.20)
+    end
+  end
+
+  let(:time_zone_normal_hour) { TimeZone.new(0.10) }
+  describe '#price_per_minute' do
+    it 'returns 0.10' do
+      expect(time_zone_normal_hour.price_per_minute).to eq(0.10)
     end
   end
 end

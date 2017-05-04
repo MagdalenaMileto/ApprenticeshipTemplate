6f7ae21ee4d550ad658d6ce90493362c69a18722
Maggie <maggiemileto@gmail.com>
2017-05-04 13:28:41 -0300

    cambios en la llamada y el facturador

--- call_billing/lib/llamada.rb
+++ call_billing/lib/llamada.rb
@@ -1,11 +1,8 @@
 class Llamada
   attr_reader :franja_horaria, :duracion
 
-  def var(tipo_facturador_llamada, franja_horaria, duracion, pais_origen, pais_destino)
-    @tipo_facturador_llamada = tipo_facturador_llamada
+  def var(franja_horaria, duracion)
     @franja_horaria = franja_horaria
     @duracion = duracion
-    @pais_origen = pais_origen
-    @pais_destino = pais_destino
   end
 end

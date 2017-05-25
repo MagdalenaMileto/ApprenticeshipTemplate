1e00e4d88282ef2095b6ba1ae73f09c28b9e5f8e
MagdalenaMileto <maggiemileto@gmail.com>
2017-05-25 19:22:44 -0300

    json_reponse

--- holiday_calendar/app/controllers/calendars_controller.rb
+++ holiday_calendar/app/controllers/calendars_controller.rb
@@ -7,13 +7,6 @@ class CalendarsController < ApplicationController
     render json: Calendar.find(calendar_id)
   end
 
-  def find_calendar_by_id
-    puts params
-    id = params[:id]
-    @calendars = Calendar.all
-    render :json => @calendars.find(id: id)
-  end
-
   private
   def calendar_name
     params[:name]
--- holiday_calendar/spec/controllers/calendars_controller_spec.rb
+++ holiday_calendar/spec/controllers/calendars_controller_spec.rb
@@ -1,32 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe CalendarsController, type: :controller do
-  context 'Given a GET request to /calendarios withouth saving' do
+  context 'Given a GET request to /calendars withouth saving' do
     it 'returns no calendar' do
       get :index
       expect(response).to have_http_status :ok
-      expect((JSON.parse response.body).count).to be(0)
+      expect(json_response.count).to eq(0)
     end
   end
 
-  context 'Given a GET request to /calendarios' do
+  context 'Given a GET request to /calendars' do
     it 'returns all calendars' do
       calendar = Calendar.create!(name: 'Argentina')
       get :index
       expect(response).to have_http_status :ok
-      expect((JSON.parse response.body)[0]['id']).to be(calendar.id)
-      expect((JSON.parse response.body).count).to be(1)
+      expect(json_response[0]['id']).to eq(calendar.id)
+      expect(json_response.count).to eq(1)
     end
   end
 
-  context 'Given a GET request to /calendarios/:name' do
-    it 'returns all calendars with that name' do
+  context 'Given a GET request to calendars?name=argentina' do
+    it 'returns all calendars wich contains that name' do
       Calendar.create!(name: 'Argentina')
       Calendar.create!(name: 'Suiza')
       Calendar.create!(name: 'Argenmexico')
       get :index, params: { name: 'Argen'}
       expect(response).to have_http_status :ok
-      expect((JSON.parse response.body).count).to be(2)
+      expect((json_response).count).to eq(2)
     end
+  end
+
+  context 'Given a GET request to calendars/id' do
+    it 'returns all calendars with that id' do
+      calendar = Calendar.create!(name: 'Argentina')
+      get :show, params: {id: calendar.id}
+      expect(response).to have_http_status :ok
+      expect(json_response).to be(name: 'Argentina', id: 1)
+    end
+  end
+
+  def json_response
+    JSON.parse response.body
   end
 end

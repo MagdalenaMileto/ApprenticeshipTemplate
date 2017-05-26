10238b13ccfc4fb1ecc976761b9bc53bbd6d6eb8
Maggie <maggiemileto@gmail.com>
2017-05-26 11:30:04 -0300

    holiday_rules controller

---
+++ holiday_calendar/app/controllers/holiday_rules_controller.rb
@@ -0,0 +1,2 @@
+class HolidayRulesController < ApplicationController
+end
--- holiday_calendar/config/routes.rb
+++ holiday_calendar/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :calendars, only: [:index, :show, :create, :update]
+  resources :holiday_rules, path: 'calendars/holiday_rules', only: [:index]
 
   #Example: get 'Route', to: 'ControllerName#ControllerAction'
   #get '/calendarios', to: 'calendars#calendars'

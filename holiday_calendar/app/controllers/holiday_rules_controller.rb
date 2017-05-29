233d9ed833076a26a0241923eb543d34c6a5e642
Maggie <maggiemileto@gmail.com>
2017-05-29 13:38:50 -0300

    funcionando si no vienen las fechas.

--- holiday_calendar/app/controllers/holiday_rules_controller.rb
+++ holiday_calendar/app/controllers/holiday_rules_controller.rb
@@ -3,11 +3,11 @@ class HolidayRulesController < ApplicationController
   def index
     calendar = Calendar.find calendar_id
     if (begins.nil? || ends.nil?)
-      begins = Date.parse(Date.new(Date.today.year, 1, 1))
-      ends = Date.parse(Date.new(Date.today.year, 12, 31))
+      begins = Date.new(Date.today.year, 1, 1)
+      ends = Date.new(Date.today.year, 12, 31)
     else
-      begins = begins()
-      ends = ends()
+      begins = Date.parse(begins())
+      ends = Date.parse(ends())
     end
       render json: calendar.holidays_between(begins, ends)
   end
@@ -18,10 +18,10 @@ class HolidayRulesController < ApplicationController
   end
 
   def begins
-    Date.parse(params[:begins])
+    params[:begins]
   end
 
   def ends
-    Date.parse(params[:ends])
+    params[:ends]
   end
 end
